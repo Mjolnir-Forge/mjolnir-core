56ae1c19a1e11bba5063daff200fc661930c20aa
Volker Hirthammer <volker.hirthammer@gmail.com>
2020-09-12 16:56:50 +0200

    Add some documentation

--- cmake/compiler_setup.cmake
+++ cmake/compiler_setup.cmake
@@ -1,4 +1,15 @@
+#[==[
+Set all compiler related cmake variables, if they are not already set.
+#]==]
+
+
+# Set compile features ------------------------------------------------------------------------------------------------
 
 if(NOT DEFINED MJOLNIR_COMPILE_FEATURES)
     set(MJOLNIR_COMPILE_FEATURES cxx_std_17)
 endif()
+
+set(MJOLNIR_COMPILE_FEATURES
+    ${MJOLNIR_COMPILE_FEATURES}
+    ${MJOLNIR_ADDITIONAL_COMPILE_FEATURES})
+
--- cmake/include_cmake_functions.cmake
+++ cmake/include_cmake_functions.cmake
@@ -1,3 +1,7 @@
+#[==[
+Include all cmake functions from the "cmake/functions" folder.
+#]==]
+
 include(functions/extract_sources_definitions_dependencies)
 include(functions/add_generic_executeable)
 include(functions/add_mjolnir_executeable)
