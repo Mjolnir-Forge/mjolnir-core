3c049cdf8782a3ac7d935c6633194126784323e2
Volker Hirthammer <volker.hirthammer@gmail.com>
2020-09-17 22:47:06 +0200

    Add include and link directories to add_generic_executable

--- cmake/functions/add_generic_executable.cmake
+++ cmake/functions/add_generic_executable.cmake
@@ -25,6 +25,12 @@ KEYWORDS
     LIBRARIES:
         Libraries that should be linked
 
+    INCLUDE_DIRECTORIES:
+        Additional directories that should be searched for the included header files (uses target_include_directories)
+
+    LINK_DIRECTORIES:
+        Additional directories that should be searched for the linked libraries (uses target_link_libraries)
+
 
 #]==]
 function(add_generic_executable target)
@@ -36,6 +42,8 @@ function(add_generic_executable target)
     set(multiValueArgs DEFINITIONS
                        SOURCES
                        LIBRARIES
+                       INCLUDE_DIRECTORIES
+                       LINK_DIRECTORIES
                        )
     cmake_parse_arguments(ARG
                           "${options}"
@@ -81,6 +89,14 @@ function(add_generic_executable target)
 
     if(DEFINED definitions)
         target_compile_definitions(${target} ${definitions})
+    endif()
+
+    if(DEFINED ARG_INCLUDE_DIRECTORIES)
+        target_include_directories(${target} ${ARG_INCLUDE_DIRECTORIES})
+    endif()
+
+    if(DEFINED ARG_LINK_DIRECTORIES)
+        target_link_directories(${target} ${ARG_LINK_DIRECTORIES})
     endif()
 
     target_link_libraries(${target} ${ARG_LIBRARIES})
