d5553b3825108669e768f56acf275a6aabed8843
Volker Hirthammer <volker.hirthammer@gmail.com>
2020-09-27 12:18:47 +0200

    Fetch gogole test only if Tests are enabled

--- cmake/dependencies/add_googletest.cmake
+++ cmake/dependencies/add_googletest.cmake
@@ -5,42 +5,45 @@ The script tries to find a system installation of googletest first. If it can't 
 GitHub repository and added as external project.
 #]==]
 
-enable_testing()
-find_package(GTest)
 
-if(NOT ${GTEST_FOUND})
-    message(STATUS "Adding local GTest installation from GitHub repository.")
+if(MJOLNIR_CORE_ENABLE_TESTS)
+    enable_testing()
+    find_package(GTest)
 
-    # Download and unpack googletest at configure time
-    configure_file(cmake/dependencies/googletest/CMakeLists.txt.in googletest-download/CMakeLists.txt)
-    execute_process(COMMAND ${CMAKE_COMMAND} -G "${CMAKE_GENERATOR}" .
-        RESULT_VARIABLE result
-        WORKING_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}/googletest-download )
-    if(result)
-        message(FATAL_ERROR "CMake step for googletest failed: ${result}")
+    if(NOT ${GTEST_FOUND})
+        message(STATUS "Adding local GTest installation from GitHub repository.")
+
+        # Download and unpack googletest at configure time
+        configure_file(cmake/dependencies/googletest/CMakeLists.txt.in googletest-download/CMakeLists.txt)
+        execute_process(COMMAND ${CMAKE_COMMAND} -G "${CMAKE_GENERATOR}" .
+            RESULT_VARIABLE result
+            WORKING_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}/googletest-download )
+        if(result)
+            message(FATAL_ERROR "CMake step for googletest failed: ${result}")
+        endif()
+        execute_process(COMMAND ${CMAKE_COMMAND} --build .
+            RESULT_VARIABLE result
+            WORKING_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}/googletest-download )
+        if(result)
+            message(FATAL_ERROR "Build step for googletest failed: ${result}")
+        endif()
+
+        # Prevent overriding the parent project's compiler/linker
+        # settings on Windows
+        set(gtest_force_shared_crt ON CACHE BOOL "" FORCE)
+
+        # Add googletest directly to our build. This defines
+        # the gtest and gtest_main targets.
+        add_subdirectory(${CMAKE_CURRENT_BINARY_DIR}/googletest-src
+                        ${CMAKE_CURRENT_BINARY_DIR}/googletest-build
+                        EXCLUDE_FROM_ALL)
+
+        # The gtest/gtest_main targets carry header search path
+        # dependencies automatically when using CMake 2.8.11 or
+        # later. Otherwise we have to add them here ourselves.
+        if (CMAKE_VERSION VERSION_LESS 2.8.11)
+            include_directories("${gtest_SOURCE_DIR}/include")
+        endif()
+        message(STATUS "Finished GTest installation.")
     endif()
-    execute_process(COMMAND ${CMAKE_COMMAND} --build .
-        RESULT_VARIABLE result
-        WORKING_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}/googletest-download )
-    if(result)
-        message(FATAL_ERROR "Build step for googletest failed: ${result}")
-    endif()
-
-    # Prevent overriding the parent project's compiler/linker
-    # settings on Windows
-    set(gtest_force_shared_crt ON CACHE BOOL "" FORCE)
-
-    # Add googletest directly to our build. This defines
-    # the gtest and gtest_main targets.
-    add_subdirectory(${CMAKE_CURRENT_BINARY_DIR}/googletest-src
-                     ${CMAKE_CURRENT_BINARY_DIR}/googletest-build
-                     EXCLUDE_FROM_ALL)
-
-    # The gtest/gtest_main targets carry header search path
-    # dependencies automatically when using CMake 2.8.11 or
-    # later. Otherwise we have to add them here ourselves.
-    if (CMAKE_VERSION VERSION_LESS 2.8.11)
-        include_directories("${gtest_SOURCE_DIR}/include")
-    endif()
-    message(STATUS "Finished GTest installation.")
 endif()
