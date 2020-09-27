5a12b4292c139468b1e0742ee170431e1f38085c
vhirtham <volker.hirthammer@gmail.com>
2020-09-27 15:40:29 +0200

    Enhance CMake setup (#8)

--- cmake/include_cmake_functions.cmake
+++ cmake/include_cmake_functions.cmake
@@ -2,10 +2,14 @@
 Include all cmake functions from the "cmake/functions" folder.
 #]==]
 
-include(functions/extract_sources_definitions_dependencies)
+include(functions/add_to_list_after_keyword)
+include(functions/process_scopes)
+include(functions/target_apply_setup)
 
 include(functions/add_generic_executable)
 include(functions/add_generic_test)
 
-include(functions/add_mjolnir_executable)
 include(functions/add_mjolnir_test)
+include(functions/extract_mjolnir_version_number)
+
+include(functions/add_mjolnir_core_test)
