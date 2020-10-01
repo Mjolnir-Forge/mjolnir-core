b3d3d6c12203b7232378415882f5a8230a98634c
Volker Hirthammer <volker.hirthammer@gmail.com>
2020-10-01 23:53:40 +0200

    Update python script and fix typos

--- scripts/python/check_clang_format_result.py
+++ scripts/python/check_clang_format_result.py
@@ -4,8 +4,9 @@ import sys
 result = subprocess.check_output(["git", "diff", "--name-only", "HEAD"]).decode("utf-8")
 
 if result:
-    modiefied_files = result.splitlines()
-    err_msg = "The following files are not formatted correctly:\n"
-    for filename in modiefied_files:
+    modified_files = result.splitlines()
+    err_msg = "FAIL - The following files are not formatted correctly:\n"
+    for filename in modified_files:
         err_msg+= f"- {filename}\n"
     sys.exit(err_msg)
+print("SUCCESS - all files formatted correctly")
