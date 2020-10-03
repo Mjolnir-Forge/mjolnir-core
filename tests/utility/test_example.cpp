22521b220c0a3ce1dbcf67db91690ca0182f46de
Volker Hirthammer <volker.hirthammer@gmail.com>
2020-10-03 13:30:23 +0200

    Define more coding conventions

--- tests/utility/test_example.cpp
+++ tests/utility/test_example.cpp
@@ -1,5 +1,11 @@
 #include <gtest/gtest.h>
 
+
+const int global_constant = 0;                // GlobalConstant
+int* const global_constant_pointer = nullptr; // GlobalConstantPointer
+int* global_pointer = nullptr;                // GlobalPointer
+int global_variable = 0;                      // GlobalVariable
+
 enum AnEnum // Enum
 {
     ONE,  // EnumConstantCase
@@ -9,53 +15,88 @@ enum AnEnum // Enum
 
 class AnAbstractClass // AbstractClass
 {
-    virtual int a_method() = 0;
+    virtual int private_method() = 0; // PrivateMethod + Method
 };
 
 class RegularClass // ClassCase
 {
     static const int class_constant = 0; // ClassConstant
-    int m_haha = 0;
+    int m_private_member = 0;            // PrivateMember + Member
+    int private_method()                 // PrivateMethod + Method
+    {
+        return 0;
+    }
+
+protected:
+    int m_protected_member = 0; // ProtectedMember + Member
+    int protected_method()      // ProtectedMethod + Method
+    {
+        return 0;
+    }
 
 public:
-    const int constant_member = 0; // ConstantMember
-    static int ne_ne;              // ClassMember
+    int public_member = 42; // PublicMember + Member
 
-    int get_haha(int* const constant_pointer_parameter) // ConstantPointerParameter
+    const int constant_member = 0; // ConstantMember
+    static int class_member;       // ClassMember
+
+    int public_method(int* const constant_pointer_parameter) // PublicMember + Method --- ConstantPointerParameter
     {
         constexpr int constant_expression_variable = 0; // ConstexprVariable
-        return m_haha + constant_member + *constant_pointer_parameter + constant_expression_variable;
+
+        return m_private_member + constant_member + *constant_pointer_parameter + constant_expression_variable
+               + global_constant;
     }
 
 
-    static bool class_method(const bool constant_parameter) // ClassMethod + ConstantParameter
+    static bool class_method(const bool constant_parameter) // ClassMethod --- ConstantParameter
     {
-        const bool return_me = constant_parameter; // Constant
-        return return_me;
+        return constant_parameter;
     }
 
-    constexpr int constant_expression_method(int a) // ConstexprMethod
+    constexpr int constant_expression_method(int method_parameter,   // ConstexprMethod --- Parameter
+                                             int* pointer_parameter) // PointerParameter
     {
-        return a + 4;
+        const int local_constant = 0;                         // LocalConstant + Constant
+        int* const local_constant_pointer = &global_variable; // LocalConstantPointer
+        int* local_pointer = pointer_parameter;               // LocalPointer
+        int local_variable = 2;                               // LocalVariable
+
+        return method_parameter + local_constant + *local_constant_pointer + *local_pointer + local_variable;
     }
 };
 
 
-constexpr int constant_expression_function(int a) // ConstexprFunction
+constexpr int constant_expression_function(int function_parameter) // ConstexprFunction --- Parameter
 {
-    return a + 4;
+    return function_parameter + 4;
 }
 
-int global_function()
+
+int* global_function() // Function + GlobalFunction
 {
-    return 5;
+    return global_constant_pointer;
 }
+
+
+template <typename... TYPE_parameters>
+bool template_function([[maybe_unused]] TYPE_parameters... parameter_pack) // Function --- ParameterPack
+{
+    return true;
+};
+
+
+namespace some_namespace // Namespace
+{
+inline namespace some_inline_namespace // InlineNamespace
+{
+}
+} // namespace some_namespace
 
 
 int add(int lhs, int rhs)
 {
-    RegularClass test_class;
-    return lhs + rhs + test_class.constant_member;
+    return lhs + rhs;
 }
 
 TEST(test, add)
