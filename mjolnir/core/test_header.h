faea1504c32f01fb8850b8cc14277f7bff822676
Volker Hirthammer <volker.hirthammer@gmail.com>
2020-10-05 19:11:45 +0200

    Add more

--- mjolnir/core/test_header.h
+++ mjolnir/core/test_header.h
@@ -4,6 +4,7 @@
 namespace mjolnir
 {
 //! @brief a function that returns 5
+//! @details this is the detailed description
 //! @return The number 5
 auto a_function() -> int
 {
@@ -16,4 +17,14 @@ auto undocumented_function() -> bool
 {
     return false;
 }
+
+
+//! @brief Some class.
+//!
+//! @details Not much more to say.
+class test_class
+{
+public:
+    int this_is_a_member = 1; //! < Some nice member
+};
 } // namespace mjolnir
