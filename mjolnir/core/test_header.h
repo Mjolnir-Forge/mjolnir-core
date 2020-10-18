30b910b5f898d6fb8f150e2a2d9d854b22a4689c
Volker Hirthammer <volker.hirthammer@gmail.com>
2020-10-18 12:52:05 +0200

    Fix added failure

--- mjolnir/core/test_header.h
+++ mjolnir/core/test_header.h
@@ -99,6 +99,8 @@ public:
     //! @details
     //!  This function is boring and doesn't do much at all
     //!
+    //! @return
+    //! A boring result
     //!
     //! @bug
     //! Is there a bug somewhere?
