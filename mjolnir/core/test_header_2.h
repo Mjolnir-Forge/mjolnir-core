ad6ebd1495dc090f734405c775bc7d94900268a0
Volker Hirthammer <volker.hirthammer@gmail.com>
2020-10-24 13:18:11 +0200

    Add cppcheck project file

--- mjolnir/core/test_header_2.h
+++ mjolnir/core/test_header_2.h
@@ -25,6 +25,18 @@ public:
     {
         return arg > 4;
     }
+
+    //! @brief
+    //! A function that contains an error that should be found by cppcheck
+    //!
+    //! @return
+    //! Some crap
+    static auto cpp_check_error() -> char*
+    {
+        static char a[10];
+        a[10] = 0;
+        return a;
+    }
 };
 
 //! @}
--- tests/utility/test_example.cpp
+++ tests/utility/test_example.cpp
@@ -2,6 +2,7 @@
 #include "mjolnir/core/test_header_2.h"
 #include <gtest/gtest.h>
 
+
 auto add(int lhs, int rhs) -> int
 {
     return lhs + rhs;
@@ -9,5 +10,8 @@ auto add(int lhs, int rhs) -> int
 
 TEST(test, add) // NOLINT cert-err58-cpp
 {
+    // char a[10];
+    // a[10] = 0;
+    AnotherClass::cpp_check_error();
     EXPECT_EQ(add(1, 2), 3);
 }
