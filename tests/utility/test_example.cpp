12dfbd915e1d3987e3590d86d68bd8c8f74bbdc4
Volker Hirthammer <volker.hirthammer@gmail.com>
2020-11-23 22:19:43 +0100

    Add address sanitizer action

--- tests/utility/test_example.cpp
+++ tests/utility/test_example.cpp
@@ -8,12 +8,20 @@ auto add(int lhs, int rhs) -> int
     return lhs + rhs;
 }
 
+
 TEST(test, add) // NOLINT cert-err58-cpp
 {
     // char a[10];
     // a[10] = 0;
     // int k = 0x7fffffff;
     // k += 2000;
-    AnotherClass::cpp_check_error();
+    // AnotherClass::cpp_check_error();
+
+    // address sanitizer
+    int* array = new int[100];
+    delete[] array;
+
+    EXPECT_EQ(add(array[2], 2), 3);
+
     EXPECT_EQ(add(1, 2), 3);
 }
