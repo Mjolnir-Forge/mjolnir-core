5f3a1cd7b93799e3a0c7ea0f383fa8330e2d0a91
Volker Hirthammer <volker.hirthammer@gmail.com>
2021-07-10 16:07:22 +0200

    Update python script

--- scripts/python/run_cmake_format.py
+++ scripts/python/run_cmake_format.py
@@ -5,19 +5,21 @@ from subprocess import DEVNULL, run
 ignore = ["build"]
 patterns = ["*.cmake", "*CMakeLists.txt"]
 
+root_dir = Path(__file__).parent.parent.parent
+config_file = f"{root_dir.absolute().as_posix()}/.cmake-format.yaml"
 
 files = []
 for pattern in patterns:
     pattern_files = [
         path.absolute().as_posix()
-        for path in Path(__file__).parent.parent.parent.rglob(pattern)
+        for path in root_dir.rglob(pattern)
         if not any(word in str(path) for word in ignore)
     ]
     files += pattern_files
 
 err_msg = ""
 for file in files:
-    r = run(["cmake-format", file, "--check"], stdout=DEVNULL, stderr=DEVNULL)
+    r = run(["cmake-format", file, "--check", "-c", config_file], stderr=DEVNULL)
     if r.returncode != 0:
         err_msg += f"The file '{file}' needs reformatting.\n"
 
