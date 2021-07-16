2f1b16b92c83c0d2b4b62b60b43ccfebdd77d70c
Volker Hirthammer <volker.hirthammer@gmail.com>
2021-07-16 19:23:18 +0200

    Update extract_mjolnir_version_number.cmake

--- cmake/functions/extract_mjolnir_version_number.cmake
+++ cmake/functions/extract_mjolnir_version_number.cmake
@@ -1,7 +1,8 @@
 #[==[
 Extract the version number of a mjolnir module from its c++ header.
 
-The returned format is MAJOR.MINOR.PATCH.TWEAK. The tweak version number is omitted if it is 0.
+The returned format is MAJOR.MINOR.PATCH.TWEAK. The tweak version number is omitted
+if it is 0.
 
 PARAMETERS:
 -----------
@@ -19,30 +20,54 @@ RETURNS
         Version number
 
 #]==]
-function(extract_mjolnir_version_number version_header_path module return_variable_name)
-    set(VERSION_REGEX "#define MJOLNIR_${module}_VERSION_(MAJOR|MINOR|PATCH|TWEAK)")
+function(
+    extract_mjolnir_version_number
+    version_header_path
+    module
+    return_variable_name)
+    set(VERSION_REGEX
+        "#define MJOLNIR_${module}_VERSION_(MAJOR|MINOR|PATCH|TWEAK)")
 
-    file(STRINGS ${version_header_path}
-            version_data
-            REGEX ${VERSION_REGEX}
-            )
+    file(STRINGS ${version_header_path} version_data REGEX ${VERSION_REGEX})
 
-    string(REGEX MATCH "VERSION_MAJOR ([0-9]*)" _ ${version_data})
+    string(
+        REGEX MATCH
+              "VERSION_MAJOR ([0-9]*)"
+              _
+              ${version_data})
     set(version_major ${CMAKE_MATCH_1})
 
-    string(REGEX MATCH "VERSION_MINOR ([0-9]*)" _ ${version_data})
+    string(
+        REGEX MATCH
+              "VERSION_MINOR ([0-9]*)"
+              _
+              ${version_data})
     set(version_minor ${CMAKE_MATCH_1})
 
-    string(REGEX MATCH "VERSION_PATCH ([0-9]*)" _ ${version_data})
+    string(
+        REGEX MATCH
+              "VERSION_PATCH ([0-9]*)"
+              _
+              ${version_data})
     set(version_patch ${CMAKE_MATCH_1})
 
-    string(REGEX MATCH "VERSION_TWEAK ([0-9]*)" _ ${version_data})
+    string(
+        REGEX MATCH
+              "VERSION_TWEAK ([0-9]*)"
+              _
+              ${version_data})
     set(version_tweak ${CMAKE_MATCH_1})
 
-    set(${return_variable_name} "${version_major}.${version_minor}.${version_patch}")
-    if (NOT ${version_tweak} EQUAL 0)
+    set(${return_variable_name}
+        "${version_major}.${version_minor}.${version_patch}")
+    if(NOT
+       ${version_tweak}
+       EQUAL
+       0)
         set(${return_variable_name} "${version_number}.${version_tweak}")
     endif()
 
-    set(${return_variable_name} ${version_number} PARENT_SCOPE)
+    set(${return_variable_name}
+        ${version_number}
+        PARENT_SCOPE)
 endfunction()
