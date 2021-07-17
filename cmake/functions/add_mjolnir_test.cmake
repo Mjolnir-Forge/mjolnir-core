1e8247a95924585e2f05f4869d4dc9bdbff8292e
Volker Hirthammer <volker.hirthammer@gmail.com>
2021-07-17 15:19:33 +0200

    Fix cmake-lint issues

--- cmake/functions/add_mjolnir_test.cmake
+++ cmake/functions/add_mjolnir_test.cmake
@@ -1,23 +1,24 @@
-#[==[
-Create a test for the mjolnir libraries and perform the setup using a keyword list.
+#[[
+Create a test for mjolnir libraries and perform the setup using a keyword list.
 
-A list of accepted keywords and their purpose is given below. Always use scope keywords
-(PUBLIC, PRIVATE, INTERFACE) directly after one of these keywords, otherwise the scope
-of the new items is undefined.
+A list of accepted keywords and their purpose is given below. Always use scope
+keywords (PUBLIC, PRIVATE, INTERFACE) directly after one of these keywords,
+otherwise the scope of the new items is undefined.
 
 
 PARAMETERS:
 -----------
 
     target:
-        Name of the CMake target that should be used. The prefix "test_" is added
-        automatically.
+        Name of the CMake target that should be used. The prefix "test_" is
+        added automatically.
 
     module:
         Name of the Mjolnir module the test belongs to.
 
     ARGN:
-        A list containing all necessary data of arbitrary length separated by keywords.
+        A list containing all necessary data of arbitrary length separated by
+        keywords.
 
 
 KEYWORDS
@@ -33,12 +34,12 @@ KEYWORDS
         Compile options that should be added (target_compile_options)
 
     INCLUDE_DIRECTORIES:
-        Additional directories that should be searched for the included header files
-        (uses target_include_directories)
+        Additional directories that should be searched for the included header
+        files (uses target_include_directories)
 
     LINK_DIRECTORIES:
-        Additional directories that should be searched for the linked libraries (uses
-        target_link_libraries)
+        Additional directories that should be searched for the linked libraries
+        (uses target_link_libraries)
 
     LINK_LIBRARIES:
         Libraries that should be linked
@@ -50,14 +51,15 @@ KEYWORDS
         List of properties (set_target_properties).
 
     SOURCES:
-        List of source files. The filepath must be given in relation to the specified
-        root directory
+        List of source files. The filepath must be given in relation to the
+        specified root directory
 
     SOURCE_DIRECTORY:
-        The root directory of the source files. If none is specified, the current CMake
-        source directory is taken.
+        The root directory of the source files. If none is specified, the
+        current CMake source directory is taken.
 
-#]==]
+]]
+# silence [C0112]
 function(add_mjolnir_test target module)
     if(${MJOLNIR_${module}_ENABLE_TESTS})
         set(test_name ${target})
