ac78448b8f8fee6a3bf5bb6422ed98ec16c092f5
Volker Hirthammer <volker.hirthammer@gmail.com>
2021-07-17 15:06:31 +0200

    Fix cmake-lint issues

--- python/check_cmake_lint.py
+++ python/check_cmake_lint.py
@@ -6,7 +6,10 @@ from sys import exit
 from mjolnir.definitions import CMAKE_CONFIG_FILE_PATH
 from mjolnir.files import get_cmake_files
 
-err_msg = "The following files contain cmake-lint issues:\n"
+err_msg = (
+    "The following files contain cmake-lint issues:\n"
+    "----------------------------------------------\n\n"
+)
 threshold = len(err_msg)
 
 for file in get_cmake_files():
