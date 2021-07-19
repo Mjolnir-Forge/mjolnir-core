c855d327484f9d59f1a2f083a22fedfebf28896d
vhirtham <volker.hirthammer@gmail.com>
2021-07-19 19:12:41 +0200

    Update test_example.cpp

--- tests/utility/test_example.cpp
+++ tests/utility/test_example.cpp
@@ -7,28 +7,3 @@
 #include <thread>
 
 
-auto test(int z) -> int
-{
-    // triggers clang-static-analyzer
-    // if (z == 0)
-    //    return 1 / z; // warn
-    return z;
-}
-
-
-auto add(int lhs, int rhs) -> int
-{
-    return lhs + rhs;
-}
-
-int        global_var = 0; // NOLINT(cppcoreguidelines-avoid-non-const-global-variables)
-std::mutex global_mutex;   // NOLINT(cppcoreguidelines-avoid-non-const-global-variables)
-
-
-auto print() -> void
-{
-    const std::lock_guard<std::mutex> lock(global_mutex);
-    global_var += 2;
-    std::cout << global_var << std::endl;
-}
-
