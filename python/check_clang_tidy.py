7f3a08ff30321e9aebb4496b29419f01498ccfdc
Volker Hirthammer <volker.hirthammer@gmail.com>
2021-07-24 12:46:01 +0200

    Adjust GH action - expect failure

--- python/check_clang_tidy.py
+++ python/check_clang_tidy.py
@@ -1,23 +1,25 @@
 """Checks all cmake files if they are correctly formatted."""
 
+from argparse import ArgumentParser
 from subprocess import DEVNULL, run
 from sys import exit
 
-from mjolnir.definitions import CMAKE_CONFIG_FILE_PATH
 from mjolnir.files import get_cpp_files
 
-version = 12
-err_msg = (
-    "The following files need reformatting:\n"
-    "--------------------------------------\n\n"
-)
-threshold = len(err_msg)
+parser = ArgumentParser()
+parser.add_argument("--version", help="Clang-tidy version", type=int, default=12)
+args = parser.parse_args()
+
+cmd = f"clang-tidy-{args.version}"
+print(f"Running {cmd}\n")
+
+error = False
 
 for file in get_cpp_files():
     print(file)
     r = run(
         [
-            f"clang-tidy-{version}",
+            f"clang-tidy-{args.version}",
             file,
             "-header-filter=.*",
             "--extra-arg=-std=gnu++20",
@@ -28,10 +30,9 @@ for file in get_cpp_files():
             "-isystem",
             "build_tidy/_deps/googletest-src/googletest/include",
         ],
-        stderr=DEVNULL,
     )
     if r.returncode != 0:
-        err_msg += f"{file}\n"
+        error = True
 
-if len(err_msg) > threshold:
-    exit(err_msg)
+if error:
+    exit(1)
