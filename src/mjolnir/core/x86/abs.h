97ea816a2285879b294c318c6b69dec6a2be1d43
Volker Hirthammer <volker.hirthammer@gmail.com>
2021-09-10 18:21:55 +0200

    Add first x86 headers

---
+++ src/mjolnir/core/x86/abs.h
@@ -0,0 +1,42 @@
+//! @file
+//! abs.h
+//!
+//! @brief
+//! Contains x86 functions to calculate the absolute values.
+
+
+#pragma once
+#include "mjolnir/core/x86/intrinsics.h"
+
+namespace mjolnir
+{
+//! \addtogroup core_x86
+//! @{
+
+//! @brief Return a register with the absolute values of the input register.
+//! @tparam T_RegisterType: The register type
+//! @param val[in]: The initial values
+//! @return Register containing the absolute values
+//! @remark source:
+//! https://stackoverflow.com/q/23847377/6700329
+template <typename T_RegisterType>
+[[nodiscard]] inline auto abs(T_RegisterType val) noexcept -> T_RegisterType;
+
+//! @}
+} // namespace mjolnir
+
+
+// ====================================================================================================================
+
+namespace mjolnir
+{
+// --------------------------------------------------------------------------------------------------------------------
+
+template <typename T_RegisterType>
+[[nodiscard]] inline auto abs(T_RegisterType val) noexcept -> T_RegisterType
+{
+    return val;
+}
+
+
+} // namespace mjolnir
---
+++ src/mjolnir/core/x86/intrinsics.h
@@ -0,0 +1,24 @@
+//! @file
+//! intrinsics.h
+//!
+//! @brief
+//! Contains generalized/template versions of the x86 intrinsics
+
+
+#pragma once
+
+#if defined __GNUC__ && __GNUC__ >= 6
+#    pragma GCC diagnostic ignored "-Wignored-attributes"
+#endif
+
+#include <x86intrin.h>
+
+
+namespace mjolnir
+{
+//! \addtogroup core_x86
+//! @{
+
+
+//! @}
+} // namespace mjolnir
