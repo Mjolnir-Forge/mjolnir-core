f6211b4849f665324fb152fd6c4b4b93a9898ec6
Volker Hirthammer <volker.hirthammer@gmail.com>
2021-10-02 13:20:46 +0200

    Fix some clang-tidy issues

--- tests/utility/test_type.cpp
+++ tests/utility/test_type.cpp
@@ -18,26 +18,26 @@ using namespace mjolnir;
 template <class T_Type>
 struct TestIsAnyOf : public ::testing::Test
 {
-    static bool expected_result;
+    static const bool expected_result;
 };
 
 
 // define test
-TYPED_TEST_SUITE_P(TestIsAnyOf);
-TYPED_TEST_P(TestIsAnyOf, test_case)
+TYPED_TEST_SUITE_P(TestIsAnyOf);     // NOLINT
+TYPED_TEST_P(TestIsAnyOf, test_case) // NOLINT
 {
     bool result = mjolnir::is_any_of<TypeParam, I32, UST, F64>();
     EXPECT_EQ(result, TestIsAnyOf<TypeParam>::expected_result);
 }
-REGISTER_TYPED_TEST_SUITE_P(TestIsAnyOf, test_case);
+REGISTER_TYPED_TEST_SUITE_P(TestIsAnyOf, test_case); // NOLINT
 
 
 // define expected results
 template <typename T_Type>
-bool TestIsAnyOf<T_Type>::expected_result =
+const bool TestIsAnyOf<T_Type>::expected_result =
         std::is_same<T_Type, I32>::value || std::is_same<T_Type, UST>::value || std::is_same<T_Type, F64>::value;
 
 
 // define test cases
 using is_any_of_test_ceses = ::testing::Types<I8, I32, I64, U32, U64, UST, F32, F64>;
-INSTANTIATE_TYPED_TEST_SUITE_P(is_any_of_test_suite, TestIsAnyOf, is_any_of_test_ceses);
+INSTANTIATE_TYPED_TEST_SUITE_P(is_any_of_test_suite, TestIsAnyOf, is_any_of_test_ceses); // NOLINT
