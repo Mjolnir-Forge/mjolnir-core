b55b8b8dcdd4fb9dc1ea7525ce268184d146ceb7
Volker Hirthammer <volker.hirthammer@gmail.com>
2021-10-06 21:53:03 +0200

    Implement mm_set1

--- src/mjolnir/core/x86/intrinsics.h
+++ src/mjolnir/core/x86/intrinsics.h
@@ -8,7 +8,7 @@
 #include "mjolnir/core/x86/constants.h"
 #include "mjolnir/core/x86/x86.h"
 
-namespace mjolnir
+namespace mjolnir::x86
 {
 //! \addtogroup core_x86
 //! @{
@@ -26,37 +26,32 @@ namespace mjolnir
 //!
 //! @return
 //! Register with broadcasted value
-template <typename T_RegisterType, typename T_Type>
-[[nodiscard]] inline auto mm_set1(T_Type value) noexcept -> T_RegisterType;
+template <typename T_RegisterType>
+[[nodiscard]] inline auto mm_set1(ValueType<T_RegisterType> value) noexcept -> T_RegisterType;
 
 
 //! @}
-} // namespace mjolnir
+} // namespace mjolnir::x86
 
 
 // ====================================================================================================================
 
-namespace mjolnir
+namespace mjolnir::x86
 {
 // --------------------------------------------------------------------------------------------------------------------
 
-template <typename T_RegisterType, typename T_Type>
-[[nodiscard]] inline auto mm_set1(T_Type value) noexcept -> T_RegisterType
+template <typename T_RegisterType>
+[[nodiscard]] inline auto mm_set1(ValueType<T_RegisterType> value) noexcept -> T_RegisterType
 {
-    /*
-        using namespace GDL::simd;
-        static_assert(IsRegisterType<_registerType>, "Function can only be used with compatible register types.");
+    static_assert(is_float_register<T_RegisterType>, "Type is not a supported float-based x86 vector register type.");
 
-        if constexpr (Is__m128<_registerType>)
-            return _mm_set1_ps(static_cast<F32>(value));
-        else if constexpr (Is__m128d<_registerType>)
-            return _mm_set1_pd(static_cast<F64>(value));
-    #ifdef __AVX2__
-        else if constexpr (Is__m256<_registerType>)
-            return _mm256_set1_ps(static_cast<F32>(value));
-        else
-            return _mm256_set1_pd(static_cast<F64>(value));
-    #endif // __AVX2__
-    */
+    if constexpr (is_m128<T_RegisterType>)
+        return _mm_set1_ps(value);
+    else if constexpr (is_m128d<T_RegisterType>)
+        return _mm_set1_pd(value);
+    else if constexpr (is_m256<T_RegisterType>)
+        return _mm256_set1_ps(value);
+    else
+        return _mm256_set1_pd(value);
 }
-} // namespace mjolnir
+} // namespace mjolnir::x86
