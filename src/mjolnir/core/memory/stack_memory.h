6e2abf44902f5135992c50844da2a5a9df0453da
Volker Hirthammer <volker.hirthammer@gmail.com>
2022-05-29 16:33:11 +0200

    Add initializetion methods

--- src/mjolnir/core/memory/stack_memory.h
+++ src/mjolnir/core/memory/stack_memory.h
@@ -9,6 +9,7 @@
 
 #include "mjolnir/core/fundamental_types.h"
 
+#include <cstddef>
 #include <memory>
 
 
@@ -20,15 +21,33 @@ namespace mjolnir
 //! @brief
 //! A stack based memory system
 //!
+//! @details
+//! This memory system manages its memory in a stack based fashion. Every allocation yields a pointer to the memory that
+//! is located directly behind the previously allocated memory block. Memory is only freed if the passed pointer during
+//! deallocated points to the last allocated memory or if the intenal allocation counter becomes 0. In the latter case,
+//! all memory is freed at once. As a consequence, deallocations should happen in the opposite order than the
+//! allocations took place (last in, first out). Otherwhise, not all memory can be freed until the number of
+//! deallocations matches the number of allocations or the memory is flushed by calling `flush`.
+//!
+//! If you know, that deallocations will occur in random order, you can disable the mechanism to free the memory of the
+//! last allocated memory block setting the template parameter `t_free_last` to `false`. This increases the speed of the
+//! deallocations.
+//!
+//! @tparam t_free_last:
+//! If `true`, memory is freed in case the passed pointer during deallocation matches the pointer to the last allocated
+//! memory block.
 //! @tparam t_thread_safe:
 //! If `true`, a thread safety mechanism is added.
-template <bool t_thread_safe = false>
+//!
+//! @todo
+//! set `t_free_last` default to `true`
+template <bool t_free_last = false, bool t_thread_safe = false>
 class StackMemory
 {
-    UST                   m_memory_size;
-    UST                   m_num_allocations;
-    U8*                   m_current_memory_ptr;
-    std::unique_ptr<U8[]> m_memory;
+    UST                          m_memory_size;
+    UST                          m_num_allocations    = {0};
+    std::byte*                   m_current_memory_ptr = {nullptr};
+    std::unique_ptr<std::byte[]> m_memory             = {nullptr};
 
 
 public:
@@ -45,10 +64,31 @@ public:
     //! @param[in] size_in_bytes:
     //! The desired size of the memory stack. Note that the memory is not allocated until the `initialize` method is
     //! called.
+    //!
+    //! @exception
+    //! `Exception` if the passed size is `0`
     explicit StackMemory(UST size_in_bytes);
 
+
 private:
+    //! @brief
+    //! Initialize the memory.
+    void deinitialize_internal();
+
+
+    //! @brief
+    //! Initialize the memory.
+    void initialize_internal();
+
+
 public:
+    //! @brief
+    //! Deinitialize the memory.
+    //!
+    //! @details
+    //! Resets the internal variables and frees the memory.
+    void deinitialize();
+
     //! @brief
     //! Get the size of the allocated memory.
     //!
@@ -58,6 +98,19 @@ public:
     //! @return
     //! Size of the memory
     [[nodiscard]] auto get_memory_size() const noexcept -> UST;
+
+
+    //! @brief
+    //! Initialize the memory.
+    void initialize();
+
+
+    //! @brief
+    //! Return `true` if the memory is initialized and `false` otherwise.
+    //!
+    //! @return
+    //! `true` or `false`
+    [[nodiscard]] auto is_initialized() const noexcept -> bool;
 };
 
 
@@ -75,12 +128,8 @@ namespace mjolnir
 {
 // --------------------------------------------------------------------------------------------------------------------
 
-template <bool t_thread_safe>
-StackMemory<t_thread_safe>::StackMemory(UST size_in_bytes)
-    : m_memory_size{size_in_bytes}
-    , m_num_allocations{0}
-    , m_current_memory_ptr{nullptr}
-    , m_memory{nullptr}
+template <bool t_free_last, bool t_thread_safe>
+StackMemory<t_free_last, t_thread_safe>::StackMemory(UST size_in_bytes) : m_memory_size{size_in_bytes}
 {
     THROW_EXCEPTION_IF(m_memory_size < 1, Exception, "Memory size can't be 0.");
 }
@@ -88,12 +137,65 @@ StackMemory<t_thread_safe>::StackMemory(UST size_in_bytes)
 
 // --------------------------------------------------------------------------------------------------------------------
 
-template <bool t_thread_safe>
-[[nodiscard]] auto StackMemory<t_thread_safe>::get_memory_size() const noexcept -> UST
+template <bool t_free_last, bool t_thread_safe>
+void StackMemory<t_free_last, t_thread_safe>::deinitialize_internal()
+{
+    THROW_EXCEPTION_IF(! is_initialized(), Exception, "Memory already deinitialized.");
+    THROW_EXCEPTION_IF(m_num_allocations > 0, Exception, "Can't deinitialize. Memory still in use.");
+
+    m_memory.reset(nullptr);
+    m_current_memory_ptr = {nullptr};
+}
+
+
+// --------------------------------------------------------------------------------------------------------------------
+
+template <bool t_free_last, bool t_thread_safe>
+void StackMemory<t_free_last, t_thread_safe>::initialize_internal()
+{
+    THROW_EXCEPTION_IF(is_initialized(), Exception, "Memory is already initialized");
+
+    m_memory.reset(new std::byte[m_memory_size]);
+    m_num_allocations    = 0;
+    m_current_memory_ptr = m_memory.get();
+}
+
+
+// --------------------------------------------------------------------------------------------------------------------
+
+template <bool t_free_last, bool t_thread_safe>
+void StackMemory<t_free_last, t_thread_safe>::deinitialize()
+{
+    deinitialize_internal();
+}
+
+
+// --------------------------------------------------------------------------------------------------------------------
+
+template <bool t_free_last, bool t_thread_safe>
+[[nodiscard]] auto StackMemory<t_free_last, t_thread_safe>::get_memory_size() const noexcept -> UST
 {
     if (m_memory)
         return m_memory_size;
     return 0;
+}
+
+
+// --------------------------------------------------------------------------------------------------------------------
+
+template <bool t_free_last, bool t_thread_safe>
+void StackMemory<t_free_last, t_thread_safe>::initialize()
+{
+    initialize_internal();
+}
+
+
+// --------------------------------------------------------------------------------------------------------------------
+
+template <bool t_free_last, bool t_thread_safe>
+[[nodiscard]] auto StackMemory<t_free_last, t_thread_safe>::is_initialized() const noexcept -> bool
+{
+    return m_memory != nullptr;
 }
 
 
--- tests/memory/test_stack_memory.cpp
+++ tests/memory/test_stack_memory.cpp
@@ -1,3 +1,4 @@
+#include "mjolnir/core/exception.h"
 #include "mjolnir/core/memory/stack_memory.h"
 #include <gtest/gtest.h>
 
@@ -11,7 +12,7 @@ using namespace mjolnir;
 
 // --- test construction ----------------------------------------------------------------------------------------------
 
-TEST(test_stack_allocator, test_construction) // NOLINT
+TEST(test_construction, check_state) // NOLINT
 {
     constexpr UST num_bytes = 1024;
 
@@ -19,12 +20,86 @@ TEST(test_stack_allocator, test_construction) // NOLINT
     // test num allocations
     // test free memory is 0
     auto mem = StackMemory(num_bytes);
+
     EXPECT_EQ(mem.get_memory_size(), 0);
+    EXPECT_FALSE(mem.is_initialized());
+}
+
+
+// --- test construction exceptions -----------------------------------------------------------------------------------
+
+TEST(test_construction, exceptions) // NOLINT
+{
+    EXPECT_THROW(StackMemory(0), Exception); // NOLINT
 }
 
 
 // --- test initialization --------------------------------------------------------------------------------------------
 
-TEST(test_stack_allocator, test_initialization) // NOLINT
+TEST(test_initialization, check_state) // NOLINT
 {
+    constexpr UST num_bytes = 1024;
+
+    auto mem = StackMemory(num_bytes);
+    mem.initialize();
+
+    EXPECT_EQ(mem.get_memory_size(), num_bytes);
+    EXPECT_TRUE(mem.is_initialized());
+
+    // todo:
+    // test num allocations
+    // test free memory is num_bytes
+}
+
+
+// --- test initialization exceptions ---------------------------------------------------------------------------------
+
+TEST(test_initialization, exceptions) // NOLINT
+{
+    constexpr UST num_bytes = 1024;
+
+    auto mem = StackMemory(num_bytes);
+    mem.initialize();
+
+    EXPECT_THROW(mem.initialize(), Exception); // NOLINT
+
+    EXPECT_EQ(mem.get_memory_size(), num_bytes);
+    EXPECT_TRUE(mem.is_initialized());
+}
+
+
+// --- test deinitialization ------------------------------------------------------------------------------------------
+
+TEST(test_deinitialization, check_state) // NOLINT
+{
+    constexpr UST num_bytes = 1024;
+
+    auto mem = StackMemory(num_bytes);
+    mem.initialize();
+    mem.deinitialize();
+
+    EXPECT_EQ(mem.get_memory_size(), 0);
+    EXPECT_FALSE(mem.is_initialized());
+
+    // todo:
+    // test num allocations
+    // test free memory 0
+}
+
+
+// --- test deinitialization ------------------------------------------------------------------------------------------
+
+TEST(test_deinitialization, exceptions) // NOLINT
+{
+    constexpr UST num_bytes = 1024;
+
+    auto mem = StackMemory(num_bytes);
+    mem.initialize();
+    mem.deinitialize();
+
+    // todo: test exception if memory still allocated
+    EXPECT_THROW(mem.deinitialize(), Exception); // NOLINT
+
+    EXPECT_EQ(mem.get_memory_size(), 0);
+    EXPECT_FALSE(mem.is_initialized());
 }
