4aa387d64a0d951959e7875dee23f07c2ca5f075
Volker Hirthammer <volker.hirthammer@gmail.com>
2022-06-05 19:04:50 +0200

    Test sized delete

--- tests/testing/test_new_delete_counter.cpp
+++ tests/testing/test_new_delete_counter.cpp
@@ -8,7 +8,10 @@ using namespace mjolnir;
 constexpr UST alignment = 32;
 struct alignas(alignment) AlignedStruct
 {
-    I32 m_member = 0;
+    I64 m_member_a = 0;
+    I64 m_member_b = 0;
+    I64 m_member_c = 0;
+    I64 m_member_d = 0;
 };
 
 
@@ -101,7 +104,6 @@ TEST(test_heap_allocation_counter_macros, new_delete_aligned_array) // NOLINT
 {
     COUNT_NEW_AND_DELETE;
 
-    // If the array is not initialized with a value, GCC behaves weird and calls new and delete very often
     auto* a = new AlignedStruct[3]; // NOLINT(cppcoreguidelines-owning-memory)
     EXPECT_NUM_NEW_AND_DELETE_EQ(1, 0);
 
@@ -130,10 +132,67 @@ TEST(test_heap_allocation_counter_macros, new_delete_aligned_array_nothrow) // N
 {
     COUNT_NEW_AND_DELETE;
 
-    // If the array is not initialized with a value, GCC behaves weird and calls new and delete very often
     auto* a = new (std::nothrow) AlignedStruct[3]; // NOLINT(cppcoreguidelines-owning-memory)
     EXPECT_NUM_NEW_AND_DELETE_EQ(1, 0);
 
     ::operator delete[](a, static_cast<std::align_val_t>(alignof(AlignedStruct)), std::nothrow);
     EXPECT_NUM_NEW_AND_DELETE_EQ(1, 1);
 }
+
+
+#ifdef __cpp_sized_deallocation
+
+// --- test sized delete with object ----------------------------------------------------------------------------------
+
+TEST(test_heap_allocation_counter_macros, sized_delete_object) // NOLINT
+{
+    COUNT_NEW_AND_DELETE;
+
+    auto* a = new I32(1); // NOLINT(cppcoreguidelines-owning-memory)
+
+    ::operator delete(a, sizeof(I32));
+    EXPECT_NUM_NEW_AND_DELETE_EQ(1, 1);
+}
+
+
+// --- test sized delete with array -----------------------------------------------------------------------------------
+
+TEST(test_heap_allocation_counter_macros, sized_delete_array) // NOLINT
+{
+    COUNT_NEW_AND_DELETE;
+
+    // If the array is not initialized with a value, GCC behaves weird and calls new and delete very often
+    auto* a = new F32[3]{0}; // NOLINT(cppcoreguidelines-owning-memory)
+
+    ::operator delete[](a, sizeof(F32[3]));
+    EXPECT_NUM_NEW_AND_DELETE_EQ(1, 1);
+}
+
+
+// --- test sized delete with object (aligned) ------------------------------------------------------------------------
+
+TEST(test_heap_allocation_counter_macros, sized_delete_aligned_object) // NOLINT
+{
+    COUNT_NEW_AND_DELETE;
+
+    auto* a = new AlignedStruct; // NOLINT(cppcoreguidelines-owning-memory)
+
+    ::operator delete(a, sizeof(AlignedStruct), static_cast<std::align_val_t>(alignof(AlignedStruct)));
+    EXPECT_NUM_NEW_AND_DELETE_EQ(1, 1);
+}
+
+
+// --- test sized delete with array (aligned) -------------------------------------------------------------------------
+
+TEST(test_heap_allocation_counter_macros, sized_delete_aligned_array) // NOLINT
+{
+    COUNT_NEW_AND_DELETE;
+
+    auto* a = new AlignedStruct[3]; // NOLINT(cppcoreguidelines-owning-memory)
+
+    ::operator delete[](a, sizeof(AlignedStruct[3]), static_cast<std::align_val_t>(alignof(AlignedStruct)));
+    EXPECT_NUM_NEW_AND_DELETE_EQ(1, 1);
+}
+
+
+#endif // __cpp_sized_deallocation
