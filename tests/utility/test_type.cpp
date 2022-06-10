d1096602d5a4a4496d7d8825018836e4c41e3ea7
Volker Hirthammer <volker.hirthammer@gmail.com>
2022-06-10 14:19:49 +0200

    test signed_to_unsigned

--- tests/utility/test_type.cpp
+++ tests/utility/test_type.cpp
@@ -8,6 +8,28 @@ using namespace mjolnir;
 
 
 // ====================================================================================================================
+// Setup
+// ====================================================================================================================
+
+template <std::signed_integral T_Type>
+class SignedTestSuite : public ::testing::Test
+{
+};
+using SignedTestTypes = ::testing::Types<I8, I16, I32, I64, IPT, int>; // NOLINT
+// cppcheck-suppress syntaxError
+TYPED_TEST_SUITE(SignedTestSuite, SignedTestTypes, );
+
+
+template <std::unsigned_integral T_Type>
+class UnsignedTestSuite : public ::testing::Test
+{
+};
+using UnsignedTestTypes = ::testing::Types<U8, U16, U32, U64, UPT, unsigned int>; // NOLINT
+// cppcheck-suppress syntaxError
+TYPED_TEST_SUITE(UnsignedTestSuite, UnsignedTestTypes, );
+
+
+// ====================================================================================================================
 // Tests
 // ====================================================================================================================
 
@@ -36,3 +58,26 @@ REGISTER_TYPED_TEST_SUITE_P(TestIsAnyOf, test_case); // NOLINT
 using IsAnyOfTestCases = ::testing::Types<I8, I32, I64, U32, U64, UST, F32, F64>;
 // cppcheck-suppress syntaxError
 INSTANTIATE_TYPED_TEST_SUITE_P(is_any_of_test_suite, TestIsAnyOf, IsAnyOfTestCases, ); // NOLINT
+
+
+// test signed_to_unsigned --------------------------------------------------------------------------------------------
+
+TYPED_TEST(SignedTestSuite, is_unsigned_with_correct_size_and_value) // NOLINT
+{
+    TypeParam i = 2;
+    auto      u = signed_to_unsigned(i);
+
+    EXPECT_TRUE(sizeof(i) == sizeof(u));
+    EXPECT_TRUE(std::is_unsigned_v<decltype(u)>);
+    EXPECT_EQ(static_cast<TypeParam>(u), i);
+}
+
+
+TYPED_TEST(UnsignedTestSuite, is_type_and_value_identical) // NOLINT
+{
+    TypeParam u1 = 2;
+    auto      u2 = signed_to_unsigned(u1);
+
+    EXPECT_TRUE((std::is_same_v<decltype(u2), TypeParam>) );
+    EXPECT_EQ(u2, u1);
+}
