55515f4d8633e1e14992ec8118b14b8a097e20e2
vhirtham <20574817+vhirtham@users.noreply.github.com>
2022-07-15 20:59:50 +0200

    Add 4x4 determinant for __m128

--- src/mjolnir/core/math/linear_algebra/determinant.h
+++ src/mjolnir/core/math/linear_algebra/determinant.h
@@ -13,6 +13,7 @@
 #include "mjolnir/core/x86/definitions.h"
 #include "mjolnir/core/x86/intrinsics.h"
 #include "mjolnir/core/x86/permutation.h"
+#include "mjolnir/core/x86/sign_manipulation.h"
 
 #include <array>
 
@@ -102,6 +103,22 @@ template <Number T_Type>
 [[nodiscard]] constexpr auto determinant_4x4(const std::array<T_Type, 16>& mat) noexcept -> T_Type;
 
 
+//! @brief
+//! Calculate the determinant of a 4x4 matrix.
+//!
+//! @tparam T_RegisterType
+//! Vector register type that represents a row or column of the matrix
+//!
+//! @param[in] mat:
+//! The matrix data as array in row-major or column-major format. Each register represents a single row/column
+//!
+//! @return
+//! Determinant of the matrix
+template <x86::FloatVectorRegister T_RegisterType>
+[[nodiscard]] inline auto determinant_4x4(const std::array<T_RegisterType, 4>& mat) noexcept
+        -> x86::ElementType<T_RegisterType>;
+
+
 //! @}
 } // namespace mjolnir
 
@@ -173,6 +190,8 @@ template <Number T_Type>
 // NOLINTNEXTLINE(readability-magic-numbers)
 [[nodiscard]] constexpr auto determinant_4x4(const std::array<T_Type, 16>& mat) noexcept -> T_Type
 {
+    // todo copy documentation from old repo
+
     T_Type ab_01 = (mat[0] * mat[5] - mat[1] * mat[4]); // NOLINT(readability-magic-numbers)
     T_Type ab_12 = (mat[1] * mat[6] - mat[2] * mat[5]); // NOLINT(readability-magic-numbers)
     T_Type ab_23 = (mat[2] * mat[7] - mat[3] * mat[6]); // NOLINT(readability-magic-numbers)
@@ -188,6 +207,55 @@ template <Number T_Type>
     T_Type cd_13 = (mat[9] * mat[15] - mat[11] * mat[13]);  // NOLINT(readability-magic-numbers)
 
     return ab_01 * cd_23 - ab_12 * cd_30 + ab_23 * cd_01 - ab_30 * cd_12 - ab_02 * cd_13 - ab_13 * cd_02;
+}
+
+
+// --------------------------------------------------------------------------------------------------------------------
+
+template <x86::FloatVectorRegister T_RegisterType>
+[[nodiscard]] inline auto determinant_4x4(const std::array<T_RegisterType, 4>& mat) noexcept
+        -> x86::ElementType<T_RegisterType>
+{
+    using namespace x86;
+    if constexpr (is_m128<T_RegisterType>)
+    {
+        // The implementation is based on the formula that expresses the 4x4 determinant by a combination of twelve 2x2
+        // determinants
+
+        // reorder values as needed
+        auto ac_b0011 = blend<0, 0, 1, 1>(mat[0], mat[2]); // for last 2 terms
+        auto ac_b1100 = blend<1, 1, 0, 0>(mat[0], mat[2]);
+        auto bd_b0011 = blend<0, 0, 1, 1>(mat[1], mat[3]);
+        auto bd_b1100 = blend<1, 1, 0, 0>(mat[1], mat[3]);
+
+        auto ac_b1100_2301 = permute<2, 3, 0, 1>(ac_b1100); // for last 2 terms
+        auto bd_b1100_2301 = permute<2, 3, 0, 1>(bd_b1100);
+        auto a_1230        = permute<1, 2, 3, 0>(mat[0]); // for first 4 terms
+        auto b_1230        = permute<1, 2, 3, 0>(mat[1]);
+        auto c_1230        = permute<1, 2, 3, 0>(mat[2]);
+        auto d_1230        = permute<1, 2, 3, 0>(mat[3]);
+
+        // calculate all twelve 2x2 determinants
+        auto abcd_45 = mm_fmsub(ac_b0011, bd_b1100_2301, mm_mul(ac_b1100_2301, bd_b0011)); // for last 2 terms
+        auto ab_03   = mm_fmsub(mat[0], b_1230, mm_mul(a_1230, mat[1]));                   // for first 4 terms
+        auto cd_03   = mm_fmsub(mat[2], d_1230, mm_mul(c_1230, mat[3]));
+
+        // reorder to multiply correct terms
+        auto abcd_45_3210   = permute<3, 2, 1, 0>(abcd_45); // for last 2 terms
+        auto cd_03_2301     = permute<2, 3, 0, 1>(cd_03);   // for first 4 terms
+        auto cd_03_2301_neg = negate_selected<0, 1, 0, 1>(cd_03_2301);
+
+        // multiply the 2x2 determinants
+        auto products_45 = mm_mul(abcd_45, abcd_45_3210);
+        auto products_03 = mm_mul(ab_03, cd_03_2301_neg);
+
+        // set redundant elements to zero and and add both products
+        products_45  = blend<0, 0, 1, 1>(products_45, mm_setzero<T_RegisterType>());
+        auto tmp_sum = mm_add(products_03, products_45);
+
+        // sum up all elements to get the determinant
+        return element_sum(tmp_sum);
+    }
 }
 
 } // namespace mjolnir
