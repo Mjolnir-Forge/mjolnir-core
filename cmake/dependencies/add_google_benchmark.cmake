1c79a567bbca828adc016fcc882c44d98876ad8a
vhirtham <20574817+vhirtham@users.noreply.github.com>
2022-09-19 19:33:04 +0200

    Disable google benchmark tests

--- cmake/dependencies/add_google_benchmark.cmake
+++ cmake/dependencies/add_google_benchmark.cmake
@@ -14,6 +14,8 @@ if(MJOLNIR_CORE_ENABLE_BENCHMARKS)
                    "installation could not be located. It will be fetched from "
                    "the GitHub repository")
 
+        set(BENCHMARK_ENABLE_TESTING OFF CACHE INTERNAL "Enable testing of the benchmark library.")
+
         FetchContent_Declare(
             benchmark
             GIT_REPOSITORY https://github.com/google/benchmark.git
