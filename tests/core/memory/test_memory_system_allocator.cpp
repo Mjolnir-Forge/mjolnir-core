4233d9aee35cbe7787b224adc6cf250b18546f26
vhirtham <20574817+vhirtham@users.noreply.github.com>
2023-01-10 21:29:12 +0100

    Comment line that causes a build error

--- tests/core/memory/test_memory_system_allocator.cpp
+++ tests/core/memory/test_memory_system_allocator.cpp
@@ -200,8 +200,8 @@ TYPED_TEST(AllocatorTestSuite, std_vector) // NOLINT
     EXPECT_EQ(vec[2], 3.F);
 
     vec.clear();
-    vec.shrink_to_fit();
     /*
+    vec.shrink_to_fit(); <--- This line causes a clang error on github. Investigate!
 
     EXPECT_EQ(mem.get_free_memory_size(), exp_memory_size);
     */
